d9b0be55b5ce6ba25b365eab847104561af9f254
Antra Das <165490591+antradas25@users.noreply.github.com>
2024-09-28 18:53:52 +0000

    20021001

---
+++ sixth.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class sixth{
+    public static void main(String[] args) {
+        Scanner Antra =new Scanner(System.in);
+        int a=Antra.nextInt();
+        int b=Antra.nextInt();
+        int c=a+b;
+        System.out.print(c);
+     
+    }
+}
